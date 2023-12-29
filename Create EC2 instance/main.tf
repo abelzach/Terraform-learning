2a077feaf27e7dd4de7c8575f09f27bee9461daa
Abel Simon Zachariah <abel@Abels-MacBook-Air.local>
2023-12-29 16:49:42 +0530

    Create EC2 instance

---
+++ Create EC2 instance/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "Test" {
+  ami = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name = "testkey"
+  tags = {
+    Name = "Abel_Test"
+  }
+}
